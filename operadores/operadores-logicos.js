b5f2c4ec40d04be41de7c6f9f8a60b044f1e79b1
Vinicius <146675540+vinimaffra03@users.noreply.github.com>
2025-01-22 20:20:23 -0300

    Add files via upload

---
+++ operadores/operadores-logicos.js
@@ -0,0 +1,25 @@
+const idade = 25;
+const possuiCarteira = true;
+
+if (idade >= 18 && possuiCarteira) {
+    console.log("Pode dirigir, possui idade suficiente e carteira de motorista.");
+} else {
+    console.log("Não pode dirigir.");
+}
+
+const temExperiencia = true;
+const temDiploma = false;
+
+if (temExperiencia || temDiploma) {
+    console.log("Pode se candidatar ao emprego, pois possui experiência ou diploma.");
+} else {
+    console.log("Não pode se candidatar ao emprego.");
+}
+
+const chuva = false;
+
+if (!chuva) {
+    console.log("Não está chovendo. Pode sair de casa.");
+} else {
+    console.log("Está chovendo. Melhor ficar em casa.");
+}
